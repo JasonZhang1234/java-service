170e394e4e32a6d789aad5a3524785980e8ac1e6
zhuangtianyu <zhuangtianyu1995@163.com>
2020-02-02 15:43:05 +0800

    commit--20200202

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+  public static void main (String[] args) {
+    int count = 10;
+    System.out.println(count);
+
+    count = 12;
+    System.out.println(count);
+
+    final int staticCount = 13;
+    System.out.println(staticCount);
+
+    int one = 1;
+    System.out.println(one == 1);
+
+  }
+}
+
+
